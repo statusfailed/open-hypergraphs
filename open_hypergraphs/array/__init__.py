5869001324473674bd05bdde3e34696332a1a8a4
statusfailed <paul@statusfailed.com>
2023-11-18 10:30:58 +0000

    remove cupy backend

--- open_hypergraphs/array/__init__.py
+++ open_hypergraphs/array/__init__.py
@@ -1,10 +1,5 @@
 """Array backends for :ref:`open_hypergraphs.finite_function`.
 
-Each sub-module of :ref:`open_hypergraphs.array` is an "array backend".
-Array backends provide a small number of *primitive functions*
-like :func:`open_hypergraphs.array.numpy.zeros` and :func:`open_hypergraphs.array.numpy.arange` .
-See :ref:`open_hypergraphs.array.numpy` (the default backend) for a list.
-
 .. warning::
    This part of the API is likely to change significantly in future releases.
 
@@ -13,5 +8,4 @@ See :ref:`open_hypergraphs.array.numpy` (the default backend) for a list.
     :recursive:
 
     open_hypergraphs.array.numpy
-    open_hypergraphs.array.cupy
 """
